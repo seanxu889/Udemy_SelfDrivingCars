501ba68f7eff6c2e795bc742b161e01871dd5918
seanxu889 <48182530+seanxu889@users.noreply.github.com>
2019-04-14 17:17:41 -0400

    Add files via upload

---
+++ 9_Self-Driving_Cars_Behavioural_Cloning/Drive.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 12:48:11 2019
+Introduce Flask & Socket.io to establish bi-directional client-server communication. 
+Ultimately, this will be done to connect our model to the simulation.
+
+sourca activate myenviron
+
+@author: seanxu
+"""
+
+import socketio
+import eventlet
+import numpy as np
+from flask import Flask
+from keras.models import load_model
+import base64
+from io import BytesIO
+from PIL import Image
+import cv2
+ 
+sio = socketio.Server()
+
+app = Flask(__name__) #'__main__'
+speed_limit = 10
+
+def img_preprocess(img):
+    img = img[60:135,:,:]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img,  (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img/255
+    return img
+ 
+@sio.on('telemetry')
+def telemetry(sid, data):
+    speed = float(data['speed'])
+    image = Image.open(BytesIO(base64.b64decode(data['image'])))
+    image = np.asarray(image)
+    image = img_preprocess(image)
+    image = np.array([image])
+    steering_angle = float(model.predict(image))
+    throttle = 1.0 - speed/speed_limit
+    print('{} {} {}'.format(steering_angle, throttle, speed))
+    send_control(steering_angle, throttle)
+ 
+
+@sio.on('connect')
+def connect(sid, environ):
+    print('Connected')
+    send_control(0, 0)
+ 
+def send_control(steering_angle, throttle):
+    sio.emit('steer', data = {
+        'steering_angle': steering_angle.__str__(),
+        'throttle': throttle.__str__()
+    })
+ 
+ 
+if __name__ == '__main__':
+    model = load_model('model.h5')
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
